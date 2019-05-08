58428888605c44ab007ed8c43e78c5f5f49cb089
ilma <ikulovac1@etf.unsa.ba>
2019-05-08 10:19:56 +0200

    drugi zadatak, tutorijal 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,42 @@
+////package com.company;
+////import java.util.Arrays;
+////import java.util.function.Function;
+////
+////public class Main {
+////
+////    public static void LambdaFunkcija (Function <Double, Double>  func) {
+////        for (double i=0;i<=100;i++) {
+////            System.out.println (func.apply(i));
+////        }
+////    }
+////
+////    public static void main (String[] args) {
+////        LambdaFunkcija (x -> x*x+2*x+1);
+////    }
+////}
+////
+////
+
 package com.company;
 
-import java.util.function.Function;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void LambdaFunkcija (Function <Double, Double>  func) {
-        for (double i=0;i<=100;i++) {
-            System.out.println (func.apply(i));
+
+    public static void main(String[] args) {
+
+        Integer brojevi[]=new Integer[] {11,12,13,14,123,41};
+
+
+        for(int i=0; i<5; i++) {
+            System.out.println (brojevi[i]);
         }
     }
 
-    public static void main (String[] args) {
-        LambdaFunkcija (x -> x*x+2*x+1);
+
+    private static int sum(int x) {
+        return x;
+
     }
 }
-
-
